af9cd252d25fdac48b425a446e288b60076bf000
nomzi3 <simonknilsson89@gmail.com>
2021-12-25 12:54:15 +0100

    day 4 - part2 v2

--- 04/02.py
+++ 04/02.py
@@ -69,19 +69,20 @@ def removeDrawnNumbFromBoard(dict_to_check,drawn_number):
     return match_found
 ###if no winner, return 0 (no such board exists)
 ###else, return the winning board for the ROW
-def checkIfWinner_Row(dict_to_check):
+def checkIfWinner_Row(dict_to_check,ig_list):
     winner_found = 0
     #print(dict_to_check)
     for i in range(1,len(dict_to_check.keys()) + 1):
         #print("number:" + str(i))
-        for y in range(0,len(dict_to_check[i].keys())):
-            for x in range(0,5):
-                if(dict_to_check[i][y][x] == "X"):
-                    #print(dict_to_check[i][y])
-                    isWinner = checkRowForWinner(dict_to_check[i][y])
-                    if(isWinner):
-                        winner_found = i
-                        return winner_found
+        if(i not in ig_list):
+            for y in range(0,len(dict_to_check[i].keys())):
+                for x in range(0,5):
+                    if(dict_to_check[i][y][x] == "X"):
+                        #print(dict_to_check[i][y])
+                        isWinner = checkRowForWinner(dict_to_check[i][y])
+                        if(isWinner):
+                            winner_found = i
+                            return winner_found
     return winner_found
 #check if the row contains only X
 ##if: return True
@@ -99,16 +100,17 @@ def checkRowForWinner(list_to_check):
 ##Iterate through the dict, if it finds a X - iterate and see if a column winner is found
 ####returns 0 if no matches
 ####else, return the ID of the board that is the winner.
-def checkIfWinner_Column(dict_to_check):
+def checkIfWinner_Column(dict_to_check,ig_list):
     winner_found = 0
     for i in range(1,len(dict_to_check.keys()) + 1):
-        for y in range(0,len(dict_to_check[i].keys())):
-            for x in range(0,5):
-                if(dict_to_check[i][y][x] == "X"):
-                    isWinner = checkColumnForWinner(dict_to_check,i,x)
-                    if(isWinner):
-                        winner_found = i
-                        return winner_found
+        if(i not in ig_list):
+            for y in range(0,len(dict_to_check[i].keys())):
+                for x in range(0,5):
+                    if(dict_to_check[i][y][x] == "X"):
+                        isWinner = checkColumnForWinner(dict_to_check,i,x)
+                        if(isWinner):
+                            winner_found = i
+                            return winner_found
     return winner_found
 ##check if a column within the board contains a winner
 ###return True if 5 matches
@@ -153,7 +155,7 @@ generated_board_dict = generateBoards(input_list)
 ##make a deep copy of our original board
 backup_board_dict = copy.deepcopy(generated_board_dict)
 
-#winner_not_yet_found = True
+ignore_list = []
 
 last_remaining_win_board_ID = 0
 last_remaining_win_Board = {}
@@ -162,25 +164,29 @@ last_winning_number = 0
 ###Go through our draw numbers, until we find a winner
 for number in draw_list:
    
-    ##if match found, check if winner generated
+    ##if any match found, check if winner generated
     match_Found = removeDrawnNumbFromBoard(generated_board_dict,number)
     if(match_Found):
-        winner_found_ROW_BoardID = checkIfWinner_Row(generated_board_dict)
+        winner_found_ROW_BoardID = checkIfWinner_Row(generated_board_dict,ignore_list)
         if(winner_found_ROW_BoardID != 0):
             print("Winner found:" + str(winner_found_ROW_BoardID))
-            print(backup_board_dict[winner_found_ROW_BoardID])
+            #print(backup_board_dict[winner_found_ROW_BoardID])
+            print(generated_board_dict[winner_found_ROW_BoardID])
             last_remaining_win_board_ID = winner_found_ROW_BoardID
             last_remaining_win_Board = copy.deepcopy(generated_board_dict[winner_found_ROW_BoardID])
             last_winning_number = number
+            ignore_list.append(winner_found_ROW_BoardID)
             #print("Final Score:" + str(calculateScore(generated_board_dict[winner_found_ROW_BoardID],number)))
 
-        winner_found_COLUMN_BoardID = checkIfWinner_Column(generated_board_dict)
+        winner_found_COLUMN_BoardID = checkIfWinner_Column(generated_board_dict,ignore_list)
         if(winner_found_COLUMN_BoardID != 0):
             print("winner found:" + str(winner_found_COLUMN_BoardID))
-            print(backup_board_dict[winner_found_COLUMN_BoardID])
+            #print(backup_board_dict[winner_found_COLUMN_BoardID])
+            print(generated_board_dict[winner_found_COLUMN_BoardID])
             last_remaining_win_board_ID = winner_found_COLUMN_BoardID
             last_remaining_win_Board = copy.deepcopy(generated_board_dict[winner_found_COLUMN_BoardID])
             last_winning_number = number
+            ignore_list.append(winner_found_COLUMN_BoardID)
             #print("Final Score:" + str(calculateScore(generated_board_dict[winner_found_COLUMN_BoardID],number)))
         
         
@@ -209,27 +215,11 @@ print("Last remaining win board ID:" + str(last_remaining_win_board_ID))
 print("Last remaining win board:")
 print(last_remaining_win_Board)
 print("Last remaining win number:" + str(last_winning_number))
-
+print("Final Score:") 
+print(calculateScore(last_remaining_win_Board,last_winning_number))
 ###TODO:
-# read file, split into functional bits.
-####part1 - drawing numbers - Array
-####part2 - gameboards - dict
-######read from newline to newline
-############if entry is completely empty (just a newline), read the next 5 lines as a gameboard. 
-############if 2 newlines in a row (empty rows), exit input-reading.
-###########################
-####game logic:
-######loop through the draw_numbers, until a winner is found (or list ends)
-#########call func1: remove the drawn number from the board - replace with X
-#########call func2: check if any winners. Split into row/column?
-##################PROBLEM - CAN MULTIPLE BOARDS WIN AT THE SAME TIME, AND DO WE CHECK FOR IT?
-#############if winner found, return and call "score-func".
-#########################################################################
 
 ####PART 2:
-###ADD OPTION TO SEE WHICH BOARD WILL BE THE LAST TO WIN
-#####ONCE WE HAVE THE BOARD, CALCULATE THE WINNING SCORE IF WE PICK THAT ONE.
-##have a var set to the board ID of the last win on the board + the board at the time that specific board hits BINGO
-######once we loop through all numbers in the list, use the above vars to calculate the winnings
-#########add a ignore list, for boards that have already won something - so we do not go through those boards again....
-
+#problems currently - it appears that it continues removing values from already won boards
+###possibly that multiple boards needs to be ignored per round, if multiple boards wins.
+###possibility - make it possible to add multiple wins per round - return a list instead
