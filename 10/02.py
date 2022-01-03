61e0eb152b29afcaac1dd0dc69b5edb7a4152f90
nomzi3 <simonknilsson89@gmail.com>
2022-01-03 19:26:51 +0100

    day 10 - part 1+2 done

--- 10/02.py
+++ 10/02.py
@@ -1,5 +1,5 @@
 fileRead_list = []
-with open("input_01.txt", "r") as f:
+with open("input_02.txt", "r") as f:
     lines = f.read().splitlines()
 
     for line in lines:
@@ -7,7 +7,7 @@ with open("input_01.txt", "r") as f:
         fileRead_list.append(line)
 print(fileRead_list)
 ##Check value of illegal character
-##Return it
+##Return its value
 ##if no match on item, return 0
 def calculateValue_illegalCharacter(item):
     if item == ')':
@@ -51,40 +51,97 @@ def checkIfMatch_closingBracket(open_bracket,closing_bracket):
 ##Else - return Value of first illegal character
 ##
 def checkLine_corrupted(single_line):
-    #print(single_line)
+    ##open bracket list - first in, first out
     open_bracket_list = []
     for item in single_line:
-        #print(item)
-        #print(checkIfMatch_openBracket(item))
-        #return_value = checkIfMatch_openBracket(item)
-        #if(return_value):
+        ##if open-bracket, insert it into position 0
         if(checkIfMatch_openBracket(item)):
-            #open_bracket_list.append(item)
             open_bracket_list.insert(0,item)
+        #closing bracket
         else:
-            #print("Closing bracket")
+            ##we should trust our input, but I don't
             if not open_bracket_list:
                 print("List is empty")
                 return 0
+            #We located an illegal closing bracket - returning the value and exiting
             elif not checkIfMatch_closingBracket(open_bracket_list[0],item):
                 print("Bracket__: " + item + " not matching__: " + open_bracket_list[0])
                 return calculateValue_illegalCharacter(item)
+            #legal closing bracket - pop it from our FIFO list and continue.
             elif checkIfMatch_closingBracket(open_bracket_list[0],item):
                 print("Closing bracket matching a Open Bracket - removing")
                 open_bracket_list.pop(0)
-        #if it matches a open-bracket, add to open-bracket list
-        ###if matching closing bracket - check last item in open-bracket list.
-        #####if matching, remove it from the open_bracket_list, and continue.
-        ###if open_bracket_list is empty, or not matching the last open bracket - return True
-        ###
-        ###if running until end of single_line, and no corrupted, return False.
-        #print(open_bracket_list)
     return 0
+#################################################################
+###############################Part2#############################
+##Return value of our incomplete character
+def calculateValue_incompleteCharacter(item):
+    if item == '(':
+        return 1
+    elif item == '[':
+        return 2
+    elif item == '{':
+        return 3
+    elif item == '<':
+        return 4
+    else:
+        return 0
+def calculate_incompleteLineScore(fifo_list):
+    total_score = 0
+    for item in fifo_list:
+        total_score = total_score * 5 + calculateValue_incompleteCharacter(item)
+    return total_score
+##Works through an incomplete line (we trust our input)
+##Returns autocomplete score of the line
+#####if 0 returned, it crashed or no extra work required. still, print the list in this case
+def checkLine_incomplete(single_line):
+    #FIFO list - first in, first out
+    open_bracket_list = []
+    for item in single_line:
+        #if open-bracket, insert into FIFO list
+        if(checkIfMatch_openBracket(item)):
+            open_bracket_list.insert(0,item)
+        ##closing bracket
+        else:
+            #we should trust our input, but I don't
+            if not open_bracket_list:
+                print("List is empty, exiting")
+                return 0
+            #Located an illegal closing bracket - should not happen in thise case
+            elif not checkIfMatch_closingBracket(open_bracket_list[0],item):
+                print("Bracket__: " + item + " not matching__: " + open_bracket_list[0])
+                return 0
+            # legal closing bracket, pop it from our FIFO list and continue
+            elif checkIfMatch_closingBracket(open_bracket_list[0],item):
+                #print("Closing bracket matching a Open Bracket - removing----")
+                open_bracket_list.pop(0)
+    print(open_bracket_list)
+    return calculate_incompleteLineScore(open_bracket_list)
 total_value = 0
+incomplete_lines = []
 for item in fileRead_list:
     print("##############################")
     return_value = checkLine_corrupted(item)
-    total_value += return_value
+    if return_value != 0:
+        total_value += return_value
+    else:
+        incomplete_lines.append(item)
+        
     #if return_value == False:
      #   print("False")
-print("Total Value:" + str(total_value))
+incomplete_value = 0
+incomplete_value_list = []
+for item in incomplete_lines:
+    return_value = checkLine_incomplete(item)
+    if return_value != 0:
+        print(return_value)
+        incomplete_value_list.append(return_value)
+        incomplete_value += return_value
+print("Total CORRUPTED Value:" + str(total_value))
+print("Total INCOMPLETE value:" + str(incomplete_value))
+print(incomplete_value_list)
+incomplete_value_list.sort()
+length_of_incomplete_list = len(incomplete_value_list)
+print(length_of_incomplete_list)
+value_to_print = int((length_of_incomplete_list-1)/2)
+print("Middle of the pack value====>" + str(incomplete_value_list[value_to_print]))
