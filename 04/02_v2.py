4605010a8d54eaaad66455e56dc11e9251538759
nomzi3 <simonknilsson89@gmail.com>
2022-02-03 03:08:31 +0100

    02_v2.py - WIP

--- 04/02_v2.py
+++ 04/02_v2.py
@@ -1,5 +1,6 @@
 import copy
-
+import pprint
+#from tokenize import Number
 
 drawing_numbers = []
 input_list = []
@@ -71,7 +72,7 @@ def removeDrawnNumbFromBoard(dict_to_check,drawn_number):
 ###else, return the winning board for the ROW
 def checkIfWinner_Row(dict_to_check,ig_list):
     winner_found = 0
-    #winner_list = []
+    winner_list = []
     #print(dict_to_check)
     for i in range(1,len(dict_to_check.keys()) + 1):
         #print("number:" + str(i))
@@ -82,9 +83,11 @@ def checkIfWinner_Row(dict_to_check,ig_list):
                         #print(dict_to_check[i][y])
                         isWinner = checkRowForWinner(dict_to_check[i][y])
                         if(isWinner):
-                            winner_found = i
-                            return winner_found
-    return winner_found
+                            winner_list.append(i)
+                            #winner_found = i
+                            #return winner_found
+    #return winner_found
+    return winner_list
 #check if the row contains only X
 ##if: return True
 ##else: return False
@@ -103,6 +106,7 @@ def checkRowForWinner(list_to_check):
 ####else, return the ID of the board that is the winner.
 def checkIfWinner_Column(dict_to_check,ig_list):
     winner_found = 0
+    winner_list = []
     for i in range(1,len(dict_to_check.keys()) + 1):
         if(i not in ig_list):
             for y in range(0,len(dict_to_check[i].keys())):
@@ -110,9 +114,11 @@ def checkIfWinner_Column(dict_to_check,ig_list):
                     if(dict_to_check[i][y][x] == "X"):
                         isWinner = checkColumnForWinner(dict_to_check,i,x)
                         if(isWinner):
-                            winner_found = i
-                            return winner_found
-    return winner_found
+                            winner_list.append(i)
+                            #winner_found = i
+                            #return winner_found
+    return winner_list
+    #return winner_found
 ##check if a column within the board contains a winner
 ###return True if 5 matches
 ###else, return False
@@ -146,7 +152,7 @@ print(drawing_numbers)
 
 ###generate our boards from the input data
 generated_board_dict = generateBoards(input_list)
-
+#pprint.pprint(generated_board_dict)
 #number_of_boards = len(generated_board_dict.keys())
 #print(number_of_boards)
 #for i in range(1,int(number_of_boards+1)):
@@ -176,52 +182,60 @@ for number in draw_list:
         ->if winner, copy down the details, and add it to the winner-list.
     If no more boards left on the board, OR, we've reached the end of the draw-list - print out the last winning board
     """
-    ##if any match found, check if winner generated
+    ##Remove number from board(s) - set as true if any number removed
     match_Found = removeDrawnNumbFromBoard(generated_board_dict,number)
+    ##if any number removed from the board - check if any winners. If so, remove them from the board.
     if(match_Found):
+
+        ##check if a winner is found in the row 
+        ###update to - return list of winners found in the ROW
+        #winner_found_ROW_BoardID.clear()
         winner_found_ROW_BoardID = checkIfWinner_Row(generated_board_dict,ignore_list)
-        if(winner_found_ROW_BoardID != 0):
-            print("Winner found:" + str(winner_found_ROW_BoardID))
-            #print(backup_board_dict[winner_found_ROW_BoardID])
-            print(generated_board_dict[winner_found_ROW_BoardID])
+        print("Row")
+        print(winner_found_ROW_BoardID)
+        if(len(winner_found_ROW_BoardID) != 0):
+            deleted_row_this_round = []
+            for winner_found_row in winner_found_ROW_BoardID:
+                if winner_found_row not in deleted_row_this_round:
+                    deleted_row_this_round.append(winner_found_row)
+                    last_remaining_win_board_ID = winner_found_row
+                    last_remaining_win_Board = copy.deepcopy(generated_board_dict[winner_found_row])
+                    last_winning_number = number
+                    del generated_board_dict[winner_found_row]
+            #print("Winner found:" + str(winner_found_ROW_BoardID))
+            
+            #print(generated_board_dict[winner_found_ROW_BoardID])
+            """
             last_remaining_win_board_ID = winner_found_ROW_BoardID
             last_remaining_win_Board = copy.deepcopy(generated_board_dict[winner_found_ROW_BoardID])
             last_winning_number = number
             ignore_list.append(winner_found_ROW_BoardID)
-            #print("Final Score:" + str(calculateScore(generated_board_dict[winner_found_ROW_BoardID],number)))
-
+            """
+        ##check if winner is found in the column
+        ###update to - return list of winners found in the column
+        #winner_found_COLUMN_BoardID.clear()
         winner_found_COLUMN_BoardID = checkIfWinner_Column(generated_board_dict,ignore_list)
-        if(winner_found_COLUMN_BoardID != 0):
-            print("winner found:" + str(winner_found_COLUMN_BoardID))
-            #print(backup_board_dict[winner_found_COLUMN_BoardID])
-            print(generated_board_dict[winner_found_COLUMN_BoardID])
+        print("Column")
+        print(winner_found_COLUMN_BoardID)
+        if(len(winner_found_COLUMN_BoardID) != 0):
+            deleted_column_this_round = []
+            for winner_found_column in winner_found_COLUMN_BoardID:
+                if winner_found_column not in deleted_column_this_round:
+                    deleted_column_this_round.append(winner_found_column)
+                    last_remaining_win_board_ID = winner_found_column
+                    last_remaining_win_Board = copy.deepcopy(generated_board_dict[winner_found_column])
+                    last_winning_number = number
+                    del generated_board_dict[winner_found_column]
+            #print("winner found:" + str(winner_found_COLUMN_BoardID))
+            
+            #print(generated_board_dict[winner_found_COLUMN_BoardID])
+            """
             last_remaining_win_board_ID = winner_found_COLUMN_BoardID
             last_remaining_win_Board = copy.deepcopy(generated_board_dict[winner_found_COLUMN_BoardID])
             last_winning_number = number
             ignore_list.append(winner_found_COLUMN_BoardID)
-            #print("Final Score:" + str(calculateScore(generated_board_dict[winner_found_COLUMN_BoardID],number)))
+            """
         
-        
-        """
-        winner_found_ROW_BoardID = checkIfWinner_Row(generated_board_dict)
-        if(winner_found_ROW_BoardID == 0):
-            print("no winner found for rows")
-        else:
-            print("Winner found - winning board:" + str(winner_found_ROW_BoardID))
-            print(backup_board_dict[winner_found_ROW_BoardID])
-            print("Final Score:" + str(calculateScore(generated_board_dict[winner_found_ROW_BoardID],number)))
-            break
-        """
-        """
-        winner_found_COLUMN_BoardID = checkIfWinner_Column(generated_board_dict)
-        if(winner_found_COLUMN_BoardID == 0):
-            print("no winner found for columns")
-        else:
-            print("winner found - winning board:" + str(winner_found_COLUMN_BoardID))
-            print(backup_board_dict[winner_found_COLUMN_BoardID])
-            print("Final Score:" + str(calculateScore(generated_board_dict[winner_found_COLUMN_BoardID],number)))
-            break
-        """
 
 print("Last remaining win board ID:" + str(last_remaining_win_board_ID))
 print("Last remaining win board:")
@@ -229,6 +243,7 @@ print(last_remaining_win_Board)
 print("Last remaining win number:" + str(last_winning_number))
 print("Final Score:") 
 print(calculateScore(last_remaining_win_Board,last_winning_number))
+
 ###TODO:
 
 ####PART 2:
